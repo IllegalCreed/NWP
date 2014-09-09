768f4d96ae9b01e1ce76e77da7bd4facc174a650
IllegalCreed <illegals0001@gmail.com>
2014-09-09 23:59:16 +0800

    创建工程

---
+++ NWPCore/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NWPCore
+{
+    /// <summary>
+    /// 场景基类
+    /// </summary>
+    public class SceneBase
+    {
+
+    }
+}
