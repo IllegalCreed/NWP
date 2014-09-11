f0d583877ec1cc25c25738716c2a6170948da84e
IllegalCreed <illegals0001@gmail.com>
2014-09-11 18:43:08 +0800

    建立界面

---
+++ NWPClient/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NWPClient.ViewModel
+{
+    public class MainWindowViewModel
+    {
+        #region 属性
+        public ObserverableCollection<>
+        #endregion
+    }
+}
