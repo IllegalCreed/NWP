6fef52c8f1117210a88640d99160ff84b4cc55a4
IllegalCreed <illegals0001@gmail.com>
2014-09-11 21:59:17 +0800

    补传converter

---
+++ NWPClient/View/Converter.cs
@@ -0,0 +1,42 @@
+﻿using NWPClient.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace NWPClient.View
+{
+    public class LogTypeToBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            Brush result = Brushes.White;
+            if(value!=DependencyProperty.UnsetValue)
+            {
+                LogType type = (LogType)value;
+                switch (type)
+                {
+                    case LogType.SYSTEM:
+                        result = Brushes.Green;
+                        break;
+                    case LogType.PLAYER:
+                        result = Brushes.Yellow;
+                        break;
+                    case LogType.ERROR:
+                        result = Brushes.Red;
+                        break;
+                }
+            }
+            return result;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
