d2f9f22b36cd9c7fba2deffb995fa535c2f7a34c
jaffrykee <jaffrykee@hotmail.com>
2014-09-12 23:40:24 +0800

    Revert "Revert "Revert "重构代码，提取脚本级任务定义"""
    This reverts commit 6e6d8933c2a7f6b9412e7d2441966a1fb42c0a0a.

--- NWPCore/GameManager.cs
+++ NWPCore/GameManager.cs
@@ -8,60 +8,16 @@ namespace NWPCore
 {
     public class GameManager
     {
-        #region 构造
-        public GameManager()
-        {
-            StatesDictionary = new Dictionary<string, GameState>();
-            Player = new Player();
-        }
-        #endregion
-
-        #region 属性
-        private string m_CurrentState;
-        public string CurrentState
-        {
-            get
-            {
-                return m_CurrentState;
-            }
-            set
-            {
-                if (value != m_CurrentState)
-                {
-                    m_CurrentState = value;
-                    GameStateChanged(StatesDictionary[m_CurrentState]);
-                }
-            }
-        }
-
-        public Dictionary<string, GameState> StatesDictionary;
+        public GameState State;
 
         public Player Player;
-
-        public event Action<GameState> GameStateChanged;
-        #endregion
-
-        #region 方法
-        public WorkCompleted Excute(string command)
-        {
-            WorkCompleted result = null;
-
-            if(StatesDictionary.ContainsKey(CurrentState))
-            {
-                GameState currentstate = StatesDictionary[CurrentState];
-                result = currentstate.Dowork(command);
-            }
-
-            return result;
-        }
-        #endregion
     }
 
-    //public enum GameState
-    //{
-    //    Beginning,
-    //    MainMenu,
-    //    CreatPlayer,
-    //    Intro
-    //}
+    public enum GameState
+    {
+        Beginning,
+        MainMenu,
+        CreatPlayer,
+        Intro
+    }
 }
