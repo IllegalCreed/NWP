26c890a408a4abf55f7b81307500cfa95750012c
IllegalCreed <illegals0001@gmail.com>
2014-09-12 23:45:31 +0800

    Revert "Revert "重构代码，提取脚本级任务定义""
    This reverts commit d35138722af84951a9170e0bbe4a6a88fd9e02f1.

---
+++ NWPCore/GameState.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NWPCore
+{
+    public class GameState : TaskBase
+    {
+        public GameState()
+        {
+
+        }
+
+        public Func<String, WorkCompleted> Dowork;
+    }
+
+    public class WorkCompleted
+    {
+        public bool result;
+        public string log;
+        public string nextState;
+    }
+
+}
